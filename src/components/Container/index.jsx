21c063663ea0bfda175e6298f3a493c057c0367f
Matheus Ramos dos Santos <mattheusdossaantos@gmail.com>
2024-03-14 14:05:20 -0300

    Updates

--- src/components/Container/index.jsx
+++
@@ -1,5 +0,0 @@
-// File to provide a container.
-
-export default function Container({ className, children }) {
-  return <div className={className}>{children}</div>;
-}
--- src/components/authentication/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-// File to provide a button element.
-
-export default function Button({ text, className, onClick }) {
-  return (
-    <button className={className} onClick={onClick}>
-      {text}
-    </button>
-  );
-}
